d76159d8d393f17aef0a8fa4d9b864e152275a9a
Ajay <106470358+PbAjay@users.noreply.github.com>
2023-01-24 10:36:52 +0530

    ᴅᴏɴᴇ

--- helper/utils.py
+++ helper/utils.py
@@ -21,7 +21,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]))
             
         tmp = progress + mr.PROGRESS_BAR.format( 
@@ -36,7 +36,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             await message.edit(
                 text="{}\n\n{}".format(ud_type, tmp),               
                 reply_markup=InlineKeyboardMarkup( [[
-                    InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")
+                    InlineKeyboardButton("ᴄᴀɴᴄᴇʟ", callback_data="cancel")
                     ]]
                 )
             )
