17177228b6960620f7fb09ea543c6f916623ca86
Ajay <106470358+PbAjay@users.noreply.github.com>
2023-01-24 10:16:58 +0530

    ᴅᴏɴᴇ

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -29,7 +29,7 @@ async def rename(bot,update):
 async def doc(bot,update):
      type = update.data.split("_")[1]
      new_name = update.message.text
-     new_filename = new_name.split(":-")[1]
+     new_filename = new_name.split(":")[1]
      file_path = f"downloads/{new_filename}"
      file = update.message.reply_to_message
      ms = await update.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ")
